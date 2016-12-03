eed4add4a2e526a7a5eac126a4c1d1072e05fc19
dunvit <sushilov.vitaly@gmail.com>
2016-12-03 10:05:27 +0200

    version 1.12 update
    version 1.12

---
+++ WormHoleLocator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using WHLocator;
+
+namespace WindowsFormsApplication3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //var listener = new AuthorizeListener();
+            //listener.Run();
+
+            log4net.Config.XmlConfigurator.Configure();
+
+            Application.Run(new WindowMonitoring());
+
+            Application.Exit(); 
+
+            
+  
+
+
+
+
+            //listener.Stop();
+        }
+    }
+}
---
+++ WormHoleLocator/Wormholes.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using log4net;
+
+namespace WHLocator
+{
+    class WormholeType
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Life { get; set; }
+    }
+
+    class Wormholes
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Wormholes));
+
+        private readonly List<WormholeType> _list = new List<WormholeType>();
+
+        public WormholeType GetWormhole(string name)
+        {
+            Log.DebugFormat("[Wormholes.GetWormhole] name is {0}", name);
+
+            return _list.FirstOrDefault(wormholeType => wormholeType.Id.Trim() == name.Trim());
+        }
+
+        public Wormholes()
+        {
+            using (var sr = new StreamReader(@"Data/Wormholes.csv"))
+            {
+                var reader = new CsvReader(sr);
+
+                //CSVReader will now read the whole file into an enumerable
+                var records = reader.GetRecords<WormholeType>();
+
+                foreach (WormholeType record in records)
+                {
+                    Log.DebugFormat("[Wormholes.Wormholes] Read csv row. {0} {1}, {2}", record.Id, record.Name, record.Life);
+
+                    _list.Add(record);
+                }
+            }
+        }
+    }
+}
